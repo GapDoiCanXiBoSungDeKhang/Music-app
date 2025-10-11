c9aaa30b9de68d4e36c0f61531eafb47161c3798
LTN130925 <ltansang374@gmail.com>
2025-10-11 14:25:34 +0000

    xây dụng thành công trang chủ đề trả về đầy đủ dữ liệu và xuất ra giao diện

---
+++ src/client/model/topic.model.ts
@@ -0,0 +1,27 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface ITopic extends Document {
+    title: string;
+    avatar?: string;
+    description?: string;
+    slug: string;
+    status: 'active' | 'inactive';
+    deleted: boolean;
+    deletedAt?: Date;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const TopicModel = new Schema<ITopic>({
+    title: { type: String, required: true },
+    avatar: String,
+    description: String,
+    slug: { type: String, trim: true, unique: true, lowercase: true },
+    status: { type: String, enum: ['active', 'inactive'], default: 'active' },
+    deleted: { type: Boolean, default: false },
+    deletedAt: Date
+}, {
+    timestamps: true
+});
+
+export const TopicModel = model<ITopic>('Topic', TopicModel, 'topics');
