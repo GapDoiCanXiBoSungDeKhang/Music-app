9d49e2f7dffb03990ac9d91cb20e23255037a8bc
LTN130925 <ltansang374@gmail.com>
2025-10-28 13:18:18 +0000

    thêm preview audio create song

--- public/admin/js/create.js
+++ public/admin/js/create.js
@@ -1,3 +1,4 @@
+// ===== Preview file image =====
 const thumbnailInput = document.getElementById('thumbnailInput');
 const previewContainer = document.getElementById('previewContainer');
 const previewImage = document.getElementById('previewImage');
@@ -19,4 +20,28 @@ removeImageBtn.addEventListener('click', () => {
     thumbnailInput.value = '';
     previewImage.src = '';
     previewContainer.style.display = 'none';
+});
+
+// ===== Preview file audio =====
+const audioInput = document.getElementById('audioInput');
+const audioPreviewContainer = document.getElementById('audioPreviewContainer');
+const audioPreview = document.getElementById('audioPreview');
+const audioRemoveBtn = document.getElementById('audioRemoveBtn');
+
+audioInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            audioPreview.src = e.target.result;
+            audioPreviewContainer.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+audioRemoveBtn.addEventListener('click', () => {
+    audioInput.value = '';
+    audioPreview.src = '';
+    audioPreviewContainer.style.display = 'none';
 });
